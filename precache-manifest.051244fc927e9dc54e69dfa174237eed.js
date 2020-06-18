135a186407f52bdb37d91a7db9aabc5ebb2b8339
Giovanna Di Meglio <reactivegio@gmail.com>
2020-06-18 13:00:17 +0200

    Updates

---
+++ precache-manifest.051244fc927e9dc54e69dfa174237eed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fefa6f734e602e3f320ff0f47277ac8",
+    "url": "/pacman-reacthook/index.html"
+  },
+  {
+    "revision": "ce9610c1bc2933e434d8",
+    "url": "/pacman-reacthook/static/css/main.0ade378c.chunk.css"
+  },
+  {
+    "revision": "d106141cc91dc84ef140",
+    "url": "/pacman-reacthook/static/js/2.bfe252d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pacman-reacthook/static/js/2.bfe252d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce9610c1bc2933e434d8",
+    "url": "/pacman-reacthook/static/js/main.66125a33.chunk.js"
+  },
+  {
+    "revision": "7f8882a83f8972553812",
+    "url": "/pacman-reacthook/static/js/runtime-main.05e48518.js"
+  }
+]);
